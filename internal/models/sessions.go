0a2ec7e6964e6da495f9045527456d7ff85192ed
sharon <ali93456@gmail.com>
2025-06-04 14:17:06 +0300

    fix: add RefreshToken field in the Session model struct

--- internal/models/sessions.go
+++ internal/models/sessions.go
@@ -3,11 +3,12 @@ package models
 import "time"
 
 type Session struct {
-	ID        int
-	Revoked   bool
-	UserAgent string
-	ExpiresAt time.Time
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	UserID    int
+	ID           int
+	Revoked      bool
+	UserAgent    string
+	RefreshToken string
+	ExpiresAt    time.Time
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	UserID       int
 }
