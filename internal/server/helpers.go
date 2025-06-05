38b350952f47c61f2330f8c227838585244ad3df
sharon <ali93456@gmail.com>
2025-06-05 19:57:03 +0300

    feat(server): add functions for header extraction, cookie setting, and token expiration calculation

--- internal/server/helpers.go
+++ internal/server/helpers.go
@@ -2,9 +2,13 @@ package server
 
 import (
 	"slices"
+	"strings"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/refine-software/afrad-api/internal/auth"
 	"github.com/refine-software/afrad-api/internal/models"
+	"github.com/refine-software/afrad-api/internal/utils"
 )
 
 func getUserRole(email string) models.Role {
@@ -41,4 +45,48 @@ func (s *Server) generateTokens(userID, role string) (access, refresh string, er
 		s.env.RefreshTokenExpInDays,
 	)
 	return
+}
+
+func getHeader(c *gin.Context, key string) string {
+	header := strings.TrimSpace(c.GetHeader(key))
+	if header == "" {
+		utils.Fail(
+			c,
+			utils.ErrHeaderMissing(key),
+			nil,
+		)
+		return ""
+	}
+	return header
+}
+
+func (s *Server) setCookie(c *gin.Context, cookieName, cookieVal string) {
+	var secure bool
+	if s.env.Environment == "prod" {
+		secure = true
+	}
+
+	expTimeInSec := int((time.Hour * 24 * time.Duration(s.env.RefreshTokenExpInDays)).Seconds())
+
+	c.SetCookie(
+		cookieName,
+		cookieVal,
+		expTimeInSec,
+		"/",
+		"",
+		secure,
+		true,
+	)
+}
+
+func (s *Server) setRefreshCookie(c *gin.Context, refreshToken string) {
+	s.setCookie(c, "refreshToken", refreshToken)
+}
+
+func getExpTimeAfterDays(numOfDays int) time.Time {
+	return time.Now().Add((time.Hour * 24) * time.Duration(numOfDays))
+}
+
+func getExpTimeAfterMins(numOfMins int) time.Time {
+	return time.Now().Add((time.Minute * time.Duration(numOfMins)))
 }
