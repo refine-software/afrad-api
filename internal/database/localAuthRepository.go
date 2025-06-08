de9e8363f37ad7d57c632119be9b79b9efd9def0
moosa <bruhgg596@gmail.com>
2025-06-08 21:12:51 +0300

    feat(localAuthRepo): add query for updating is_account_verified column to true

--- internal/database/localAuthRepository.go
+++ internal/database/localAuthRepository.go
@@ -7,13 +7,17 @@ import (
 
 type LocalAuthRepository interface {
 	// create JWT based authentication,
-	// columns required: user_id, phone_number, password_hash.
+	// columns required: user_id, password_hash.
 	Create(ctx *gin.Context, db Querier, l *models.LocalAuth) error
 
 	// update user local auth,
 	// required columns: is_phone_verified, password_hash,
 	// by user_id.
 	Update(ctx *gin.Context, db Querier, l *models.LocalAuth) error
+
+	// update is_account_verified column to true,
+	// by user_id.
+	UpdateIsAccountVerifiedToTrue(ctx *gin.Context, db Querier, userID int32) error
 }
 
 type localAuthRepo struct{}
@@ -24,10 +28,10 @@ func NewLocalAuthRepository() LocalAuthRepository {
 
 func (r *localAuthRepo) Create(ctx *gin.Context, db Querier, l *models.LocalAuth) error {
 	query := `
-		INSERT INTO local_auth(user_id, phone_number, password_hash)
+		INSERT INTO local_auth(user_id, password_hash)
 		VALUES ($1, $2, $3)
 	`
-	_, err := db.Exec(ctx, query, l.UserID, l.PhoneNumber, l.PasswordHash)
+	_, err := db.Exec(ctx, query, l.UserID, l.PasswordHash)
 	if err != nil {
 		return Parse(err)
 	}
@@ -41,6 +45,23 @@ func (r *localAuthRepo) Update(ctx *gin.Context, db Querier, l *models.LocalAuth
 		WHERE user_id = $1
 	`
 	_, err := db.Exec(ctx, query, l.UserID, l.IsAccountVerified, l.PasswordHash)
+	if err != nil {
+		return Parse(err)
+	}
+	return nil
+}
+
+func (r *localAuthRepo) UpdateIsAccountVerifiedToTrue(
+	ctx *gin.Context,
+	db Querier,
+	userID int32,
+) error {
+	query := `
+		UPDATE local_auth
+		SET is_account_verified = true
+		WHERE user_id = $1
+	`
+	_, err := db.Exec(ctx, query, userID)
 	if err != nil {
 		return Parse(err)
 	}
