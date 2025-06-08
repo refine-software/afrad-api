8f6c9c251c5e5ef3718e2b12cd389c285fea0626
sharon <ali93456@gmail.com>
2025-06-08 18:42:47 +0300

    feat(sessionRepo): add 2 methods for revoking all session and getting all sessions

--- internal/database/sessionRepository.go
+++ internal/database/sessionRepository.go
@@ -14,13 +14,24 @@ type SessionRepository interface {
 		userAgent string,
 	) (models.Session, error)
 
+	// Fetch all sessions of a certain user
+	GetAllOfUser(
+		ctx *gin.Context,
+		db Querier,
+		userID int32,
+	) ([]models.Session, error)
+
 	// This method will create a session, the following columns are required:
 	// user_id, user_agent, refresh_token, expires_at.
 	Create(ctx *gin.Context, db Querier, sess *models.Session) error
+
 	// Update the user session with the following columns:
 	// revoked, refresh_token, expires_at.
 	// the session will be updated using its id.
 	Update(ctx *gin.Context, db Querier, sess *models.Session) error
+
+	// This method will revoke all sessions of a certain user
+	RevokeAllOfUser(ctx *gin.Context, db Querier, userID int32) error
 }
 
 type sessionRepo struct{}
@@ -56,6 +67,38 @@ func (sessionRepo *sessionRepo) GetByUserIDAndUserAgent(
 	return s, Parse(err)
 }
 
+func (s *sessionRepo) GetAllOfUser(
+	ctx *gin.Context,
+	db Querier,
+	userID int32,
+) ([]models.Session, error) {
+	query := `
+	SELECT id, revoked, user_agent, refresh_token, expires_at, created_at, updated_at, user_id
+	FROM sessions
+	WHERE user_id = $1
+	`
+
+	rows, err := db.Query(ctx, query, userID)
+	if err != nil {
+		return nil, Parse(err)
+	}
+	defer rows.Close()
+
+	var sessions []models.Session
+	for rows.Next() {
+		var s models.Session
+		if err = rows.Scan(&s.ID, &s.Revoked, &s.UserAgent, &s.RefreshToken, &s.ExpiresAt, &s.CreatedAt, &s.UpdatedAt, &s.UserID); err != nil {
+			return nil, err
+		}
+		sessions = append(sessions, s)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, Parse(err)
+	}
+
+	return sessions, nil
+}
+
 func (s *sessionRepo) Create(ctx *gin.Context, db Querier, sess *models.Session) error {
 	query := `
 	INSERT INTO sessions(user_id, user_agent, refresh_token, expires_at)
@@ -75,4 +118,19 @@ func (s *sessionRepo) Update(ctx *gin.Context, db Querier, sess *models.Session)
 
 	_, err := db.Exec(ctx, query, sess.ID, sess.Revoked, sess.RefreshToken, sess.ExpiresAt)
 	return Parse(err)
+}
+
+func (s *sessionRepo) RevokeAllOfUser(ctx *gin.Context, db Querier, userID int32) error {
+	query := `
+		UPDATE sessions
+		SET revoked = true
+		WHERE user_id = $1
+	`
+
+	_, err := db.Exec(ctx, query, userID)
+	if err != nil {
+		return Parse(err)
+	}
+
+	return nil
 }
