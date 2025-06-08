2c1eb6aa5421a6f820a6a1961d6b10e99b95cb67
sharon <ali93456@gmail.com>
2025-06-08 15:11:17 +0300

    refactor: SendVerificationEmail func shouldn't be in jwt.go file

---
+++ internal/auth/email.go
@@ -0,0 +1,25 @@
+package auth
+
+import (
+	"github.com/refine-software/afrad-api/config"
+	"gopkg.in/gomail.v2"
+)
+
+func SendVerificationEmail(email, OTP string, env *config.Env) error {
+	htmlBody := generateTemplate(OTP)
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", env.Email)
+	m.SetHeader("To", email)
+	m.SetHeader("Subject", "Refine Budget Account Verification")
+	m.SetBody("text/html", htmlBody)
+
+	d := gomail.NewDialer(
+		"smtp.hostinger.com",
+		465,
+		env.Email,
+		env.Password,
+	)
+	d.SSL = true
+	return d.DialAndSend(m)
+}
--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -6,9 +6,7 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/refine-software/afrad-api/config"
 	"github.com/refine-software/afrad-api/internal/utils"
-	"gopkg.in/gomail.v2"
 )
 
 func GenerateToken(tokenSecret string, expInMin int) (string, error) {
@@ -117,23 +115,4 @@ func ParseRefreshToken(token, refreshTokenSecret string) (claims *jwt.Registered
 	}
 
 	return claims, nil
-}
-
-func SendVerificationEmail(email, OTP string, env *config.Env) error {
-	htmlBody := generateTemplate(OTP)
-
-	m := gomail.NewMessage()
-	m.SetHeader("From", env.Email)
-	m.SetHeader("To", email)
-	m.SetHeader("Subject", "Refine Budget Account Verification")
-	m.SetBody("text/html", htmlBody)
-
-	d := gomail.NewDialer(
-		"smtp.hostinger.com",
-		465,
-		env.Email,
-		env.Password,
-	)
-	d.SSL = true
-	return d.DialAndSend(m)
 }
