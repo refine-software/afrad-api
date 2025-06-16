e599741c6f884fe7278cad5b6ee0fa26274c1bc1
sharon <ali93456@gmail.com>
2025-06-16 21:21:21 +0300

    feat: add JSON tags to model fields in productModels for improved serialization

--- internal/models/productModels.go
+++ internal/models/productModels.go
@@ -7,62 +7,62 @@ import (
 )
 
 type Product struct {
-	ID              int32
-	Name            string
-	Details         pgtype.Text
-	Thumbnail       string
-	CreatedAt       time.Time
-	UpdatedAt       time.Time
-	BrandID         int32
-	ProductCategory int32
+	ID              int32       `json:"id"`
+	Name            string      `json:"name"`
+	Details         pgtype.Text `json:"details"`
+	Thumbnail       string      `json:"thumbnail"`
+	CreatedAt       time.Time   `json:"-"`
+	UpdatedAt       time.Time   `json:"-"`
+	BrandID         int32       `json:"-"`
+	ProductCategory int32       `json:"-"`
 }
 
 type ProductVariant struct {
-	ID        int32
-	Quantity  int
-	Price     int
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	ProductID int32
-	ColorID   int32
-	SizeID    int32
+	ID        int32     `json:"id"`
+	Quantity  int       `json:"quantity"`
+	Price     int       `json:"price"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+	ProductID int32     `json:"-"`
+	ColorID   int32     `json:"-"`
+	SizeID    int32     `json:"-"`
 }
 
 type Category struct {
-	ID       int32
-	Name     string
-	ParentID pgtype.Int4
+	ID       int32       `json:"id"`
+	Name     string      `json:"name"`
+	ParentID pgtype.Int4 `json:"-"`
 }
 
 type RatingReview struct {
-	ID        int32
-	Rating    int32
-	Review    pgtype.Text
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	UserID    int32
-	ProductID int32
+	ID        int32       `json:"id"`
+	Rating    int32       `json:"rating"`
+	Review    pgtype.Text `json:"review"`
+	CreatedAt time.Time   `json:"createdAt"`
+	UpdatedAt time.Time   `json:"updatedAt"`
+	UserID    int32       `json:"-"`
+	ProductID int32       `json:"-"`
 }
 
 type Image struct {
-	ID          int32
-	Image       string
-	LowResImage string
-	ProductID   int32
+	ID          int32  `json:"id"`
+	Image       string `json:"image"`
+	LowResImage string `json:"lowResImage"`
+	ProductID   int32  `json:"-"`
 }
 
 type Brand struct {
-	ID    int32
-	Brand string
+	ID    int32  `json:"id"`
+	Brand string `json:"brand"`
 }
 
 type Size struct {
-	ID    int32
-	Size  string
-	Label string
+	ID    int32  `json:"id"`
+	Size  string `json:"size"`
+	Label string `json:"label"`
 }
 
 type Color struct {
-	ID    int32
-	Color string
+	ID    int32  `json:"id"`
+	Color string `json:"color"`
 }
